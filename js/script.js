a8e75cb4cd3adfb9467e43eea7ef4eac0715c11e
Habibur Rahman <habibur88454@gmail.com>
2024-09-22 23:40:14 +0600

    added teo button event hanlder and calculate value

--- js/script.js
+++ js/script.js
@@ -26,3 +26,38 @@ document.getElementById('btn-donation').addEventListener('click', function () {
     btnHistory.classList.remove('bg-[#B4F461]', 'py-3', 'px-8', 'rounded-lg', 'font-medium', 'text-xl');
     btnHistory.classList.add('border-2', 'py-3', 'px-8', 'rounded-lg', 'font-medium', 'text-xl')
 })
+
+// donate now 1
+document.getElementById('donate-now-1').addEventListener('click', function () {
+    const donate1Value = parseFloat(getInputFieldValueById('donate-value-1').value)
+
+    const donate1DefaultValue = parseFloat(getTextFieldValueById('donate-1-default-value').innerText);
+
+    const amountTotal = donate1DefaultValue + donate1Value;
+    const donate1Element = document.getElementById('donate-1-default-value');
+    donate1Element.innerText = amountTotal;
+
+    const myBalance = parseFloat(getTextFieldValueById('my-balance').innerText);
+    const latestBalance = myBalance - donate1Value;
+
+    const myBalanceElement = document.getElementById('my-balance');
+    myBalanceElement.innerText = latestBalance;
+})
+
+// donate now 2
+document.getElementById('donate-now-2').addEventListener('click', function () {
+    const donate2Value = parseFloat(getInputFieldValueById('donate-value-2').value)
+
+    const donate2DefaultValue = parseFloat(getTextFieldValueById('donate-2-default-value').innerText);
+  
+    const amountTotal = donate2DefaultValue + donate2Value;
+    const donate2Element = document.getElementById('donate-2-default-value')
+    donate2Element.innerText = amountTotal;
+
+    const myBalance = parseFloat(getTextFieldValueById('my-balance').innerText);
+    const latestBalance = myBalance - donate2Value;
+
+    const myBalanceElement = document.getElementById('my-balance');
+    myBalanceElement.innerText = latestBalance;
+    
+})
--- js/utility.js
+++ js/utility.js
@@ -3,3 +3,10 @@ function getInputFieldValueById(id) {
     const fieldValue = document.getElementById(id);
     return fieldValue;
 }
+
+
+// text field value
+function getTextFieldValueById(id) {
+    const textFieldValue = document.getElementById(id);
+    return textFieldValue;
+}
