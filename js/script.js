c54563371f87d6ff8eb9f58546eca288c1f92a51
Habibur Rahman <habibur88454@gmail.com>
2024-09-23 00:55:51 +0600

    input field validation

--- js/script.js
+++ js/script.js
@@ -31,32 +31,63 @@ document.getElementById('btn-donation').addEventListener('click', function () {
 document.getElementById('donate-now-1').addEventListener('click', function () {
     const donate1Value = parseFloat(getInputFieldValueById('donate-value-1').value)
 
+    // check value is not a number
+    if (isNaN(donate1Value)) {
+        return alert('Provide a valid number')
+    }
+    // check value less then 0
+    if (donate1Value < 0) {
+        return alert('Please provide a positive value')
+    }
+
+
     const donate1DefaultValue = parseFloat(getTextFieldValueById('donate-1-default-value').innerText);
 
     const amountTotal = donate1DefaultValue + donate1Value;
     const donate1Element = document.getElementById('donate-1-default-value');
-    donate1Element.innerText = amountTotal;
+
 
     const myBalance = parseFloat(getTextFieldValueById('my-balance').innerText);
     const latestBalance = myBalance - donate1Value;
 
+    // check main balance more then donate
+    if (myBalance < donate1Value) {
+        return alert('Do not have that much money')
+    }
+
+    donate1Element.innerText = amountTotal;
     const myBalanceElement = document.getElementById('my-balance');
     myBalanceElement.innerText = latestBalance;
+
 })
 
 // donate now 2
 document.getElementById('donate-now-2').addEventListener('click', function () {
     const donate2Value = parseFloat(getInputFieldValueById('donate-value-2').value)
 
+    // check value is not a number
+    if (isNaN(donate2Value)) {
+        return alert('Provide a valid number')
+    }
+    // check value less then 0
+    if (donate2Value < 0) {
+        return alert('Please provide a positive value')
+    }
+
     const donate2DefaultValue = parseFloat(getTextFieldValueById('donate-2-default-value').innerText);
 
     const amountTotal = donate2DefaultValue + donate2Value;
     const donate2Element = document.getElementById('donate-2-default-value')
-    donate2Element.innerText = amountTotal;
 
     const myBalance = parseFloat(getTextFieldValueById('my-balance').innerText);
     const latestBalance = myBalance - donate2Value;
 
+    // check main balance more then donate
+    if (myBalance < donate2Value) {
+        return alert('Do not have that much money')
+    }
+
+    donate2Element.innerText = amountTotal;
     const myBalanceElement = document.getElementById('my-balance');
     myBalanceElement.innerText = latestBalance;
 
@@ -66,15 +97,29 @@ document.getElementById('donate-now-2').addEventListener('click', function () {
 document.getElementById('donate-now-3').addEventListener('click', function () {
     const donate3Value = parseFloat(getInputFieldValueById('donate-value-3').value);
 
+    // check value is not a number
+    if (isNaN(donate3Value)) {
+        return alert('Provide a valid number')
+    }
+    // check value less then 0
+    if (donate3Value < 0) {
+        return alert('Please provide a positive value')
+    }
+
     const donate3DefaultValue = parseFloat(getTextFieldValueById('donate-3-default-value').innerText);
 
     const amountTotal = donate3DefaultValue + donate3Value;
     const donate3Element = document.getElementById('donate-3-default-value');
-    donate3Element.innerText = amountTotal;
 
     const myBalance = parseFloat(getTextFieldValueById('my-balance').innerText);
     const latestBalance = myBalance - donate3Value;
 
+    // check main balance more then donate
+    if (myBalance < donate3Value) {
+        return alert('Do not have that much money')
+    }
+
+    donate3Element.innerText = amountTotal;
     const myBalanceElement = document.getElementById('my-balance');
     myBalanceElement.innerText = latestBalance;
 })
